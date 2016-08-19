a59b2d0a27dba70dcec3fe5efbbefe22e88459fe
joshxjin <joshxjin@gmail.com>
2016-08-19 14:42:48 +1200

    Initial commit.

---
+++ ConwaysGoL/src/Frontend/Application.java
@@ -0,0 +1,17 @@
+package Frontend;
+
+import javafx.stage.Stage;
+
+public class Application extends javafx.application.Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
